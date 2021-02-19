f2de31a2496c833335b32d02e6a03fa4d8dd07ea
Simon Thompson <simonct.sct@gmail.com>
2021-02-19 13:37:21 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+cocktail_names = %w[sidecar 'old fashioned' 'gin fizz' mojito martini margerita 'aperol spritz' manhattan]
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+cocktails = []
+ingredients = []
+
+cocktail_names.each do |cocktail_name|
+  cocktails << Cocktail.create(name: cocktail_name)
+end
+
+50.times do
+  ingredients << Ingredient.create(name: Faker::Food.ingredient)
+end
+
+cocktails.each do |cocktail|
+  Dose.create(cocktail: cocktail, ingredient: ingredients.sample, description: Faker::Food.measurement)
+end
